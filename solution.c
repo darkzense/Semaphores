22883a3e7ecfed2aaea7a0af2dbbfe9d8dc156bb
darkzense <torresjohn094@gmail.com>
2022-10-19 15:44:29 -0600

    code cleanup

--- solution.c
+++ solution.c
@@ -2,23 +2,29 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/sem.h>
+#define PARENT_SEM 0
+#define CHILD_SEM 1
+#define ITERATIONS 3
 
 void spam(const char* letter, int times);
+void lock_sem(int sem_id, int sem_num);
+void release_sem(int sem_id, int sem_num);
 
 int main(int argc, char *argv[])
 {
     int pid, semid;
+    // Buffer de operaciones del semaforo
     struct sembuf sb = {0, -1, 0};
+
+    // Se crea el semaforo
     if ((semid = semget(IPC_PRIVATE, 2, IPC_CREAT|0600)) == -1)
     {
         perror("semget");
         exit(1);
     }
 
-    /* Se crea un proceso hijo */
-    pid = fork();
     /* Se comprueba que no hubo error en fork() */
-    if (pid == -1)
+    if ((pid = fork()) == -1)
     {
         perror("fork");
         exit(-1);
@@ -26,52 +32,26 @@ int main(int argc, char *argv[])
 
     if (pid != 0)
     { /* Proceso Padre */
-        for (int i=0; i<3; i++) {
-            /* Comienzo de la sección critica: */
+        for (int i=0; i<ITERATIONS; i++) {
+            /* *** Comienzo de la sección critica: *** */
             spam("+", 10);
-            /* Fin de la sección critica */
-
-            sb.sem_num = 0;
-            sb.sem_op = 1;
+            /* *** Fin de la sección critica *** */
             // Semaforo en verde para el hijo
-            if (semop(semid, &sb, 1) == -1) 
-            { 
-                perror("semop");
-                exit(1);
-            }
-
-            sb.sem_num = 1;
-            sb.sem_op = -1;
+            release_sem(semid, CHILD_SEM);
             // Semaforo en rojo para el padre
-            if (semop(semid, &sb, 1) == -1) 
-            { 
-                perror("semop");
-                exit(1);
-            }
+            lock_sem(semid, PARENT_SEM);
         }
     }
     else
     { /* Proceso Hijo */
-        for (int i=0; i<3; i++) {
-            sb.sem_num = 0;
-            sb.sem_op = -1;
+        for (int i=0; i<ITERATIONS; i++) {
             // Semaforo en rojo para el hijo
-            if (semop(semid, &sb, 1) == -1) 
-            { 
-                perror("semop");
-                exit(1);
-            }
-            /* Comienzo de la sección critica: */
+            lock_sem(semid, CHILD_SEM);
+            /* *** Comienzo de la sección critica: *** */
             spam("o", 10);
-            /* Fin de la sección critica */
-            sb.sem_num = 1;
-            sb.sem_op = 1;
+            /* *** Fin de la sección critica *** */
             // Semaforo en verde para el padre
-            if (semop(semid, &sb, 1) == -1) 
-            { 
-                perror("semop");
-                exit(1);
-            }
+            release_sem(semid, PARENT_SEM);
         }
     }
 }
@@ -81,6 +61,28 @@ void spam(const char* letter, int times)
     for (int j = 0; j < times; j++)
     {
         write(STDOUT_FILENO, letter, 1);
-        sleep(1);
     }
 }
+
+void sem_set(int sem_id, struct sembuf* sb, int sem_num, int sem_op) {
+    sb->sem_num = sem_num;
+    sb->sem_op = sem_op;
+    sb->sem_flg = 0;
+    // Semaforo en verde para el padre
+    if (semop(sem_id, sb, 1) == -1) 
+    { 
+        perror("semop");
+        exit(1);
+    }
+}
+
+void lock_sem(int sem_id, int sem_num) {
+    struct sembuf sb;
+    sem_set(sem_id, &sb, sem_num, -1);
+}
+
+void release_sem(int sem_id, int sem_num) {
+    struct sembuf sb;
+    sem_set(sem_id, &sb, sem_num, 1);
+}
+
